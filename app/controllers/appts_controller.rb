af9ac02260ddeea0f9eb3a1d039ef4e68c8cac94
Scott (DC)  Christensen <schristensen10@gmail.com>
2020-09-02 18:21:02 -0600

    full app works

--- app/controllers/appts_controller.rb
+++ app/controllers/appts_controller.rb
@@ -2,26 +2,27 @@ class ApptsController < ApplicationController
   before_action :set_patient
 
   def index
-    @appointments = @patient.appointments.all
+    @appointments = @patient.appts.all
   end
 
   def new
-    @appointment = @patient.appointments.new
+    @appointment = @patient.appts.new
     @doctors = Doctor.all
   end
 
   def create
-    @appointment = @patient.appointments.new(appt_params)
+    @appointment = @patient.appts.new(appt_params)
     @doctors = Doctor.all
 
-    if @appointment.save
+    if @appointment.save 
       redirect_to patient_appts_path(@patient)
     else
       render :new
+    end
   end
 
   def destroy
-    @appointment = @patient.appointments.find(params[:id])
+    @appointment = @patient.appts.find(params[:id])
     @appointment.destroy
     redirect_to patient_appts_path(@patient)
   end
@@ -32,6 +33,6 @@ class ApptsController < ApplicationController
     end
 
     def appt_params
-      params.require(:appointment).permit(:doctor_id, :date)
+      params.require(:appt).permit(:doctor_id, :date)
     end
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,5 +1,5 @@
 class DoctorsController < ApplicationController
-  before_action :set_doctor, only: [:show, :edit, :destroy]
+  before_action :set_doctor, only: [:show, :destroy]
 
   def index
     @doctors = Doctor.all
